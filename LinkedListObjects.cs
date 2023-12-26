beb47c934bc9edf5206f82f4d6483987cd647666
Azhar Amir Kimanje <77221185+kimanjea@users.noreply.github.com>
2023-12-26 02:53:58 -0800

    Implemented the hashtable functionality successfully
    Next we add the unique ID functionality for each name added so that we can meet the format of (Key, value) insertions and look ups

---
+++ LinkedListObjects.cs
@@ -0,0 +1,144 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace HashTables_in_C_;
+class Node {
+        public string? data { get; set; } // this is how we get out values 
+        public Node? Next {get; set;} // this is how we are able to use the reference property
+
+        public string? playerdescription {get; set;} // add all these to function
+        public int? playerposition {get; set;} // add all these to functions
+        public void Display(){ // we use this to print the data in our nodes
+            Console.WriteLine(data);
+        }
+
+    }
+class LinkedListOps{ // carries out linkedlist operations
+
+        public Node? head; //we use this to access the head of the list
+
+        public void InsertAtTop(string data){ // inserting from the top
+            Node? current = head; //we use current so that we dont update the head
+            Node newNode = new Node(); //we cretae a new node to hold host our new data
+            newNode.data=data; 
+
+            if(current!=null) {
+                newNode.Next = head;// this means that new node references head, then head can be assigned to new node
+                head = newNode;//
+            } else {
+                newNode.Next = head; 
+                head = newNode;    
+            }
+        }
+
+        public void DeleteAtTop() {//deleting at the top
+           Node? current = head;
+           Node? temp = new Node(); //we create temp to hold our data when deleting from head
+
+           temp = head?.Next;
+           head= temp;
+                    
+        }
+
+        public void InsertAtMiddle(string data, string newdata){ //inserting frm the middle of linkedlist
+            Node? current = head;
+            Node? prev = new Node();//prev helps us access previous node 
+            Node? newNode = new Node();
+            newNode.data = newdata;
+
+            if(current?.data != data) {
+                prev = current; //we are able to access previous node by assigning prev to current
+                current = current?.Next;
+            }
+
+            newNode.Next = current;
+            #pragma warning disable CS8602
+            prev.Next = newNode;
+            
+
+        }
+
+        public void DeleteFromMiddle(string data){ // deleting from the middle of linkedList
+            Node? current = head;
+            Node prev = new Node();
+
+            while(current?.data != data){
+                #pragma warning disable CS8600
+                prev =  current;
+                current = current?.Next;
+            }
+
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+        prev.Next = current?.Next;
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+
+    }
+
+        public void InsertATBottom(string data) { //inserting from the bottom linkedlist 
+            Node? current = head;
+            Node newNode = new Node();
+            newNode.data = data;
+            Node prev = new Node();
+
+
+            while (current!= null){
+                prev = current;
+                current = current.Next;
+            }
+
+            prev.Next = newNode; 
+        }
+
+        public void DeleteAtBottom (){ //this method deletes from the bottom of the list
+            Node? current = head;
+            Node prev = new Node();
+            
+            while(current?.Next!= null){
+                prev = current;
+                current= current.Next;
+            }
+            prev.Next = null;
+        }
+
+        public void Delete(string data){ // delete from anywhere 
+            Node? current = head;
+            Node? prev = new Node();
+
+            if(current?.data==data){
+                Node? temp = new Node(); //we create temp to hold our data when deleting from head
+                temp = head?.Next;
+                head= temp;
+
+            } else {
+                while(current?.data != data){
+                #pragma warning disable CS8600
+                prev =  current;
+                current = current?.Next;
+                 }
+                #pragma warning disable CS8602 
+                prev.Next = current?.Next;
+            }
+        }
+
+        public void GetObject(string name){
+            Node? current = head;
+            if(current!=null){
+                while(current?.data!= name){
+                    current = current?.Next;
+                }
+                Console.Write(current.data);
+            } else {
+                Console.WriteLine($"{name} is not in the linkedlist in hashtable");
+            }
+            
+        }
+        
+        public void PrintLinkedList(){// we use this method to print out our new list whenever we perform on it
+            Node? current = head;
+
+            while(current!= null) {
+                current.Display();
+                current = current.Next;
+            }
+            
+        }
+}
---
+++ Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Net.Http.Headers;
+using System.Reflection.Metadata;
+using System.Threading.Tasks.Dataflow;
+using HashTables_in_C_;
+
+namespace hashes {
+    class Program{
+        static void Main(string [] args) { //hashtables are great for Constant time complexity key value look ups in large data sets
+
+        // we initialize a hashtable name student that we will use as our dictionary to store names  
+            Hashingtable StudentList = new Hashingtable("list");
+            StudentList.Insert("azhar"); // we use the Insert function to add items to our hashtable
+            StudentList.Insert("Ruth");
+            StudentList.Insert("Tai");
+            StudentList.Insert("najibs");
+            StudentList.Insert("najib");
+            StudentList.Insert("Azhar");
+            StudentList.Insert(")=");
+            StudentList.Insert("letsd go");
+            StudentList.Insert("Jerrohe hrfieir");
+            StudentList.Insert("h");
+            StudentList.Insert("Kimanje!st");
+            
+            Console.WriteLine();Console.WriteLine();Console.WriteLine();
+
+            Console.WriteLine("We test out our Get function here to find name najibs");
+            StudentList.get("najibs"); // we use the get to look for items in our hashtable
+           
+            Console.WriteLine();Console.WriteLine();Console.WriteLine();
+         
+            StudentList.PrintHashtable();  //we use the printhashtable to print our dictionary of items
+        
+            Console.WriteLine();Console.WriteLine();Console.WriteLine();
+            Console.WriteLine("We test out our Remove function here to find name Azhar");
+            StudentList.remove("azhar"); // we use the remove function to remove items 
+            Console.WriteLine();
+            Console.WriteLine("Final hashtable");
+            StudentList.PrintHashtable(); // we print out our new hashtable her;.
+
+
+            Console.WriteLine();Console.WriteLine();Console.WriteLine();
+            Console.WriteLine("We test out our Remove function here to find name Jerrohe hrfieir");
+            StudentList.remove("Jerrohe hrfieir");
+            Console.WriteLine();
+            Console.WriteLine("Final hashtable");
+            StudentList.PrintHashtable();
+
+
+
+        
+        }
+
+
+        class Hashingtable{ // we define our class hashingtable
+           public LinkedListOps [] people = new LinkedListOps[10]; // we make an array that holds linkedlist objects
+           public string? Name {get; set;} // we use this to get the name of our linkedlist
+           public Hashingtable(string name){ // we set up a contructor but that is not necessary in this context, I did it just for formality
+                Name= name;     
+           }
+
+           public int hashingfunction(string name){ // a good hashfunction is vital and it helps with creating indexes that fit our array size and returns an index for insertion
+                int hashCode= name.Length % people.Length; // this algorithm calculates our index using modula to keep it within the array size
+            return hashCode;
+           }
+
+           public bool Insert(string name){ // Takes in a name to be added in the dictionary
+           //we use this function to insert values into our hashingtable 
+
+                int hashtableIndex= hashingfunction(name); //first we get our index using the hashfunction
+                LinkedListOps newObject = new LinkedListOps(); // we create a new linked list object to use to place in our array
+                    
+
+                if(people[hashtableIndex]==null){ // check for emptiness of index
+                     people[hashtableIndex]=newObject; //assign the index with our object
+                      people[hashtableIndex].InsertAtTop(name); // we insert name into our object
+                     return true; //we return true after success
+
+                }  else if (people[hashtableIndex]!=null) { //if not empty
+                    //people[hashtableIndex]=newObject;
+                     people[hashtableIndex].InsertAtTop(name); // we automatically add to the top of our linklist object at that index
+                    return true; //we return true for success
+
+                } else {
+                    Console.WriteLine("failed to add linkedlist object");
+                    return false; // we ever fail but i highly doubt
+                }
+               
+           }
+
+           public bool remove(string name){ // we use the remove function to remove from hashtabe
+                int hastableIndex = hashingfunction(name); //again we start by calling our hashfunction
+
+                if(people[hastableIndex]==null){ //if the location is null then it is automatically not in our dictionary
+                    Console.WriteLine("Name is not in list");
+                    return false; //we return false immediately
+                } else { // if not then
+                    Console.Write("Removed the name {");
+                    people[hastableIndex].Delete(name);
+                    Console.WriteLine( "} from hashtable");
+                    return true;
+                }
+           }
+           
+           public bool get(string name){ // we use the get function to do a key value look up of O(1)
+                int hastableIndex = hashingfunction(name); //again we start by calling our hashfunction
+
+                if(people[hastableIndex]==null){ //if the location is null then it is automatically not in our dictionary
+                    Console.WriteLine("Name is not in list");
+                    return false; //we return false immediately
+                } else { // if not then
+                    Console.Write("Found the name {");
+                    people[hastableIndex].GetObject(name);
+                    Console.WriteLine( "} in hashtable");
+                    return true;
+                }
+           }
+
+           public void PrintHashtable(){ //we use this function to print out our entire hashtable
+            Console.WriteLine("***  Our hashtable indexes to value  ****");
+                for(int i =0; i<people.Length; i++){
+                    
+                    if(people[i]!=null){
+                        Console.Write($"{i} =>  "); people[i].PrintLinkedList();
+                    } else {
+                        Console.WriteLine($"{i} => Null");
+                    }
+                  
+                }
+           }
+
+        }
+    }
+}
